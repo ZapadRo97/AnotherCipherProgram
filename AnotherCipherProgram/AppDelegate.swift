9203c639860a8d97f32e5b9b19f14465aaaf7e9f
Purcea Florin <zapad.daniel7@gmail.com>
2020-04-27 21:06:33 +0300

    Initial Commit

---
+++ AnotherCipherProgram/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AnotherCipherProgram
+//
+//  Created by Florin Daniel on 27/04/2020.
+//  Copyright © 2020 Florin Daniel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AnotherCipherProgram/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AnotherCipherProgram
+//
+//  Created by Florin Daniel on 27/04/2020.
+//  Copyright © 2020 Florin Daniel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
